63661dda0304055687bbab1030814edd7fb2c6bf
AnastasiaFedotova <anastaisafedotova@gmail.com>
2020-03-17 00:26:03 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -13,10 +13,40 @@ class Navigation {
 }
 
 class Slider {
-  constructor(slider) {
-    this.slider = slider;
+  constructor(slider, countSlider) {
+    this.slider = document.querySelector(slider);
+    this.count = countSlider;
+    this.position = 1;
+    this.slider.querySelector(".slider__btn_next").addEventListener("click", () => this.goNext.call(this));
+    this.slider.querySelector(".slider__btn_back").addEventListener("click", () => this.goBack.call(this))
+  }
+
+  goNext() {
+    let sliderBack = this.position;
+    this.position + 1 > this.count ? this.position = 1 : this.position++;
+    this.draw(this.position, sliderBack);
+  }
+
+  goBack() {
+    let sliderBack = this.position;
+    this.position - 1 < 1 ? this.position = this.count : this.position--;
+    this.draw(this.position, sliderBack);
+  }
+
+  draw(count, countBack) {
+    this.slider.classList.remove(`slider_${countBack}`);
+    this.slider.classList.add(`slider_${count}`);
+  }
+}
+
+class Mobile {
+  constructor() {
+
   }
 }
 
 
 let navSingolo = new Navigation("nav", "focus");
+let navPortfolio = new Navigation(".portfolio__nav", "focus");
+let slider = new Slider(".slider", 2);
+let mobile1 = new Mobile();
